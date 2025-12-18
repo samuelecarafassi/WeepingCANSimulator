c769a85319c3f6db102f525926b9ae2ccd50bc8d
Leonardo Beleffi <leonardo.beleffi@studio.unibo.it>
2025-12-18 20:18:04 +0100

    UPDATE - add alternating read-write interaction

--- can_bus.py
+++ can_bus.py
@@ -19,21 +19,41 @@ class CanECU():
     
     def __init__(self, canController: CanController, can_id: int, time_out:float):
         self.__canController = canController
+        self.__bus = canController.get_bus()
         self.__isRunning = False
         self.__id = can_id
         self.__timeout = time_out
 
-    def send_message(self,data:list):
+    def send_message(self, data: bytearray):
         msg = can.Message(arbitration_id=self.__id, data=data, is_extended_id=False)
-        self.__canController.get_bus().send(msg)
+        self.__bus.send(msg)
 
     def __ecu_action(self):
+        # alternate send and receive
+        i = 0 if self.__start else 1
+
         while self.__isRunning:
-            self.send_message([random.randint(0,250)])
+            if i == 0:
+                msg = random.randint(0,250).to_bytes()
+                print(f"{self.__id} -> {msg}")
+                self.send_message(msg)
+            else:
+                """ DOES'T WORK. DON'T KNOW WHY
+                print("here")
+                with self.__bus as bus:
+                    for msg in bus:
+                        print(f"{self.__id} -in- {msg.data}")
+                print("here not")
+                """
+                msg = bytes(self.__bus.recv().data)
+                print(f"{self.__id} <- {msg}")
+
+            i = (i + 1) % 2
             time.sleep(self.__timeout)
 
-    def start_ecu(self):
+    def start_ecu(self, start):
         self.__isRunning = True
+        self.__start = start
         print(f"Starting ECU {self.__id}...")
         t = threading.Thread(target=self.__ecu_action, daemon=True)
         t.start()
@@ -47,15 +67,16 @@ if __name__ == "__main__":
         canc1 = CanController('vcan0', 'socketcan')
         ecu1 = CanECU(canc1,100,0.1)
         ecu2 = CanECU(CanController('vcan0', 'socketcan'),200,0.2)
-        ecu1.start_ecu()
-        ecu2.start_ecu()
+        ecu1.start_ecu(start=True)
+        ecu2.start_ecu(start=False)
 
         # Read what is happening on the bus
         bus = can.interface.Bus('vcan0', interface='socketcan')
         try:
             while True:
-                msg = bus.recv()
-                if msg is not None:
-                    print(f"ID: {hex(msg.arbitration_id)}, Data: {msg.data}, Timestamp: {msg.timestamp}")
+                if False:
+                    msg = bus.recv()
+                    if msg is not None:
+                        print(f"ID: {hex(msg.arbitration_id)}, Data: {msg.data}, Timestamp: {msg.timestamp}")
         except KeyboardInterrupt:
             print("Stopping ECUs")
